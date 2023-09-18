115e587b8d95ac3f087fdd98e806914260763d79
Salimo0 <salim.chaaki@gmail.com>
2023-09-18 10:06:54 +0100

    my pro 0.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState } from 'react';
 
 const App = () => {
   const [person, setPerson] = useState({
@@ -10,64 +9,52 @@ const App = () => {
   });
 
   const [show, setShow] = useState(true);
-
   const [countdown, setCountdown] = useState(60);
 
   const toggleShow = () => {
     setShow(!show);
   };
 
-  useEffect(() => {
-    let intervalId;
-
+  const startCountdown = () => {
     if (countdown > 0) {
-      intervalId = setInterval(() => {
-        setCountdown((prevCountdown) => prevCountdown - 1);
+      setTimeout(() => {
+        setCountdown(countdown - 1);
+        startCountdown(); 
       }, 1000);
     }
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [countdown]);
+  };
 
   return (
-      <div style={{ backgroundColor: 'lightblue', padding: '20px', border: '1px solid #ccc', textAlign: 'center', }} className="div">
-        <button
-          style={{
-            backgroundColor: 'blue',
-            color: 'white',
-            padding: '10px',
-            borderRadius: '5px',
-          }}
-          onClick={toggleShow}
-        >
-          Toggle Profile
-        </button>
-    
-        {show && (
-          <div>
-            <h2>{person.firstName}</h2>
-            <p>{person.bio}</p>
-            <img src={person.imgSrc} alt="Profile" style={{
-              maxWidth: '100%',
-              borderRadius: '50%', 
-              width: '150px', 
-              height: '150px', 
-            }} />
-            <p style={{ fontWeight: 'bold' }}>Profession: {person.profession}</p>
-          </div>
-        )}
-    
-        <p>Countdown: {countdown} seconds</p>
-      </div>
-    );
-    
+    <div style={{ backgroundColor: 'lightblue', padding: '20px', border: '1px solid #ccc', textAlign: 'center' }} className="div">
+      <button
+        style={{
+          backgroundColor: 'blue',
+          color: 'white',
+          padding: '10px',
+          borderRadius: '5px',
+        }}
+        onClick={toggleShow}
+      >
+        Toggle Profile
+      </button>
+
+      {show && (
+        <div>
+          <h2>{person.firstName}</h2>
+          <p>{person.bio}</p>
+          <img src={person.imgSrc} alt="Profile" style={{
+            maxWidth: '100%',
+            borderRadius: '50%',
+            width: '150px',
+            height: '150px',
+          }} />
+          <p style={{ fontWeight: 'bold' }}>Profession: {person.profession}</p>
+        </div>
+      )}
+
+      <p>Countdown: {countdown} seconds</p>
+    </div>
+  );
 };
 
 export default App;
-
-
-
-
-
